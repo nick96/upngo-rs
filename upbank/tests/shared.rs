c285d652077501c331999e5fe890d11d290bee9f
Nick Spain <nicholas.spain@stileeducation.com>
2020-09-19 11:06:55 +1000

    Add tests for various transaction filters

--- upbank/tests/shared.rs
+++ upbank/tests/shared.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Datelike, Duration, Timelike, Utc};
 use std::sync::Once;
 use upbank::Client;
 
@@ -20,4 +21,16 @@ pub fn get_client() -> Client {
 
 pub fn get_bad_client() -> Client {
     Client::default_client("not-a-real-token".to_string())
+}
+
+pub fn start_of_month() -> DateTime<Utc> {
+    Utc::now()
+        .with_day(1)
+        .unwrap()
+        .with_hour(0)
+        .unwrap()
+        .with_minute(0)
+        .unwrap()
+        .with_second(0)
+        .unwrap()
 }
--- upbank/tests/transaction_test.rs
+++ upbank/tests/transaction_test.rs
@@ -1,18 +1,74 @@
 mod shared;
 
-use shared::{init_logger, get_client};
+use shared::{get_client, init_logger, start_of_month};
 
 #[test]
-fn test_list_transactions() {
+fn test_transactions_list() {
     init_logger();
     let transactions = get_client().transaction.list().exec().unwrap();
     assert!(transactions.is_ok());
 }
 
 #[test]
-fn test_get_transaction() {
+fn test_transaction_get() {
     init_logger();
     let id = std::env::var("UPBANK_TRANSACTION_ID").unwrap();
     let transaction = get_client().transaction.get(id).unwrap();
     assert!(transaction.is_ok());
+}
+
+#[test]
+fn test_transaction_list_pagination() {
+    init_logger();
+    let transactions = get_client().transaction.list().size(1).exec().unwrap();
+    assert!(transactions.is_ok());
+    use upbank::response::Response;
+    match transactions {
+        Response::Ok(ts) => assert_eq!(ts.data.len(), 1),
+        Response::Err(_) => unreachable!(),
+    }
+}
+
+#[test]
+fn test_transaction_list_filter_since() {
+    init_logger();
+    let start_of_month = start_of_month();
+    let transactions = get_client()
+        .transaction
+        .list()
+        .since(start_of_month)
+        .exec()
+        .unwrap();
+    assert!(transactions.is_ok());
+    use upbank::response::Response;
+    let ts = match transactions {
+        Response::Ok(ts) => ts,
+        Response::Err(_) => unreachable!(),
+    };
+    assert!(!ts.data.is_empty());
+    for t in ts.data {
+        assert!(t.attributes.created_at >= start_of_month);
+    }
+}
+
+#[test]
+fn test_transaction_list_filter_until() {
+    init_logger();
+    let start_of_month = start_of_month();
+    let transactions = get_client()
+        .transaction
+        .list()
+        .until(start_of_month)
+        .exec()
+        .unwrap();
+    assert!(transactions.is_ok());
+    use upbank::response::Response;
+    let ts = match transactions {
+        Response::Ok(ts) => ts,
+        Response::Err(_) => unreachable!(),
+    };
+    assert!(!ts.data.is_empty());
+    for t in ts.data {
+        assert!(t.attributes.created_at < start_of_month);
+    }
 }
